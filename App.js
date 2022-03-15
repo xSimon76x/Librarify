84d5a0040bbd1d874e7870e76ce5766dfbedbb34
SimonIvan <73963276+xSimon76x@users.noreply.github.com>
2022-03-14 23:53:27 -0300

    introduccion y base de aplicacion movil

--- App.js
+++ App.js
@@ -10,49 +10,21 @@ import React from 'react';
 import { NavigationContainer } from "@react-navigation/native"
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from "./src/view/Home"
-
-
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Library from "./src/view/Library"
 
 const Stack = createStackNavigator();
 
 const App = () => {
 
-
-
-
   return (
 
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-
-
-
+        <Stack.Screen name="Home" component={Home} options={{ title: 'Inicio' }} />
+        <Stack.Screen name="Library" component={Library} />
       </Stack.Navigator>
-
-
-
     </NavigationContainer>
   );
 };
-
-
 
 export default App;
--- src/view/Home.js
+++ src/view/Home.js
@@ -1,10 +1,25 @@
 import React from "react"
-import { Text, View } from "react-native"
+import { Text, View, TouchableHighlight } from "react-native"
 
-export default function Home() {
+const BOOK_LIST = [
+    {
+        id: 1,
+        title: 'Hacia rutas salvajes',
+    },
+    {
+        id: 2,
+        title: 'El nombre del viento',
+    }
+]
+
+export default function Home({ navigation }) {
+
     return (
         <View>
             <Text> Soy Home</Text>
+            <TouchableHighlight onPress={() => navigation.navigate('Library')}>
+                <Text> hacia library</Text>
+            </TouchableHighlight>
         </View>
     );
 };
