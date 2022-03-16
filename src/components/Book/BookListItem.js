dcfdb6fe0fae67195c1cc098aca85a5af4343c91
SimonIvan <73963276+xSimon76x@users.noreply.github.com>
2022-03-15 22:03:29 -0300

    integracion de lista de libros

--- src/components/Book/BookListItem.js
+++ src/components/Book/BookListItem.js
@@ -1,5 +1,12 @@
-export default function BookListItem() {
+import React from "react"
+import { Text, View, TouchableHighlight } from "react-native"
 
+export default function BookListItem({ book }) {
+    return (
+        <View>
+            <Text>{book.title}</Text>
+        </View>
+    )
 };
 
 
--- src/view/Home.js
+++ src/view/Home.js
@@ -1,5 +1,6 @@
-import React from "react"
-import { Text, View, TouchableHighlight } from "react-native"
+import React from 'react';
+import { Text, View, TouchableHighlight, FlatList } from 'react-native';
+import BookListItem from '../components/Book/BookListItem';
 
 const BOOK_LIST = [
     {
@@ -9,17 +10,29 @@ const BOOK_LIST = [
     {
         id: 2,
         title: 'El nombre del viento',
-    }
-]
+    },
+    {
+        id: 3,
+        title: 'Extraño de la ceguera',
+    },
+];
 
 export default function Home({ navigation }) {
-
     return (
         <View>
-            <Text> Soy Home</Text>
+            {/* <Text> Soy Home</Text>
             <TouchableHighlight onPress={() => navigation.navigate('Library')}>
                 <Text> hacia library</Text>
-            </TouchableHighlight>
+            </TouchableHighlight> */}
+            <FlatList
+                data={BOOK_LIST}
+                renderItem={({ item }) => <BookListItem book={item} />}
+                keyExtractor={item => item.id}
+                ListHeaderComponent={
+                    <View>
+                        <Text>---Mi lista de libros---</Text>
+                    </View>
+                }></FlatList>
         </View>
     );
-};
+}
